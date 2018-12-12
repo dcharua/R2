a7a57102d20efd4203f56d3f6b4e5230d6746c54
dcharua <danielcharua@hotmail.com>
2018-12-11 20:54:56 -0600

    login fix

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('base', 'forms', 'ui', 'home', 'tables', 'data', 'additional', 'base', 'egresos'):
+    for module_name in ('base', 'home', 'egresos'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
