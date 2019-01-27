29250c50e8f2abd6d38cb3932b677edf3ac809e2
Bazbaz <abazbaz@amazon.com>
2019-01-26 21:56:43 -0800

    cambie admistracion/route.py para que cada subpagina tenga su propia funcion en vez de templates

--- app/administracion/routes.py
+++ app/administracion/routes.py
@@ -32,6 +32,7 @@ def clientes():
     
     cur = db.connect_to_db("select * from colores where coldescripcion like '%NEGR%' ORDER BY COLNUMERO")
     print(type(cur))
+    print(cur.head())
      
 
 
@@ -46,39 +47,39 @@ def clientes():
                            velocity_max = 1)
 #
 #    
-#@blueprint.route('beneficiarios', methods=['GET', 'POST'])
-#def beneficiarios():
-#    formaPago = list(['Banamex','Santander','BBVA'])
-#
-#    user_inputs = dict(request.form)
-#    print(user_inputs)
-#    return render_template("beneficiarios.html",
-#                           navbar_data_capture = 'active',
-#                           title = "Registro de Egresos",
-#                           formaPago = formaPago,
-#                           vendor = vendor,
-#                           proveedor = proveedor,
-#                           categoria = categoria,
-#                           concepto = concepto,
-#                           velocity_max = 1)
-#    
-#    
-#@blueprint.route('cuentas', methods=['GET', 'POST'])
-#def cuentas():
-#    formaPago = list(['Banamex','Santander','BBVA'])
-#    vendor = list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
-#    proveedor = list(['sub_categoria_1','sub_categoria_2','sub_categoria_3','sub_categoria_4','sub_categoria_5'])
-#    categoria = list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
-#    concepto= list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
-#    user_inputs = dict(request.form)
-#    print(user_inputs)
-#
-#    return render_template("cuentas.html",
-#                           navbar_data_capture = 'active',
-#                           title = "Registro de Egresos",
-#                           formaPago = formaPago,
-#                           vendor = vendor,
-#                           proveedor = proveedor,
-#                           categoria = categoria,
-#                           concepto = concepto,
-#                           velocity_max = 1)
+@blueprint.route('beneficiarios', methods=['GET', 'POST'])
+def beneficiarios():
+    formaPago = list(['Banamex','Santander','BBVA'])
+
+    user_inputs = dict(request.form)
+    print(user_inputs)
+    return render_template("beneficiarios.html",
+                           navbar_data_capture = 'active',
+                           title = "Registro de Egresos",
+                           formaPago = formaPago,
+                           vendor = vendor,
+                           proveedor = proveedor,
+                           categoria = categoria,
+                           concepto = concepto,
+                           velocity_max = 1)
+    
+    
+@blueprint.route('cuentas', methods=['GET', 'POST'])
+def cuentas():
+    formaPago = list(['Banamex','Santander','BBVA'])
+    vendor = list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
+    proveedor = list(['sub_categoria_1','sub_categoria_2','sub_categoria_3','sub_categoria_4','sub_categoria_5'])
+    categoria = list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
+    concepto= list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
+    user_inputs = dict(request.form)
+    print(user_inputs)
+
+    return render_template("cuentas.html",
+                           navbar_data_capture = 'active',
+                           title = "Registro de Egresos",
+                           formaPago = formaPago,
+                           vendor = vendor,
+                           proveedor = proveedor,
+                           categoria = categoria,
+                           concepto = concepto,
+                           velocity_max = 1)
