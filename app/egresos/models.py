668d6dc52bb9e5efb5d346900e7fcba3ca020f6c
dcharua <danielcharua@hotmail.com>
2019-01-29 16:26:15 -0600

    CR pago perfil backend

--- app/egresos/models.py
+++ app/egresos/models.py
@@ -21,16 +21,17 @@ class Egresos(db.Model):
     __table_args__ = {'extend_existing': True}
 
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    beneficiario = Column(String(20))
     fecha_vencimiento = Column(Date)
     fecha_programada_pago = Column(Date)
     numero_documento = Column(String(20))
     monto_total = Column(Numeric)
     referencia = Column(String(40))
-    empresa = Column(String(40))
     comentario = Column(String(200))
     detalles = relationship("DetallesEgreso")
+    beneficiario_id= Column(Integer, ForeignKey('beneficiarios.id'))
+    empresa_id = Column(Integer, ForeignKey('empresas.id'))
     pagos = relationship("Pagos", secondary=egresos_has_pagos)
+
 
     def __repr__(self):
         return '<Egreso {}>'.format(self.id)    
@@ -43,10 +44,10 @@ class DetallesEgreso(db.Model):
     __table_args__ = {'extend_existing': True}
 
     id = Column(Integer, primary_key=True)
-    centro_negocios = Column(String(20))
-    proveedor = Column(String(50))
-    categoria = Column(String(50))
-    concepto = Column(String(50))
+    centro_negocios_id = Column(Integer, ForeignKey('centros_negocio.id'))
+    proveedor_id = Column(Integer, ForeignKey('beneficiarios.id'))
+    categoria_id = Column(Integer, ForeignKey('categorias.id'))
+    concepto_id = Column(Integer, ForeignKey('conceptos.id'))
     monto = Column(Numeric)
     numero_control = Column(String(200))
     descripcion = Column(String(200)) 
@@ -61,14 +62,14 @@ class Pagos(db.Model):
     __table_args__ = {'extend_existing': True}
 
     id = Column(Integer,  unique=True, nullable=False, primary_key=True)
-    forma_pago = Column(String(20))
     referencia_pago = Column(String(20))
     fecha_pago = Column(Date)
     conciliado = Column(Boolean)
     referencia_conciliacion= Column(String(20))
-    empresa = Column(String(20))
     monto_total = Column(Numeric)
     comentario = Column(String(200))
+    cuenta_id = Column(Integer, ForeignKey('cuentas.id'))
+    forma_pago_id = Column(Integer, ForeignKey('formas_pago.id'))
     beneficiario_id= Column(Integer, ForeignKey('beneficiarios.id'))
 
     def __repr__(self):
@@ -87,10 +88,12 @@ class Beneficiarios(db.Model):
     cuenta_banco = Column(String(20))
     banco = Column(String(30))
     saldo = Column(Numeric) 
-    egreso_id= Column(Integer, ForeignKey('egresos.id'))
+    egresos = relationship("Egresos")
+    pagos = relationship("Pagos")
+   
 
     def __repr__(self):
-        return '<Beneficiario {}>'.format(self.nombre) 
+        return self.nombre 
 
     def get_beneficiario_by_id(self, id):
         print(id)
@@ -110,9 +113,78 @@ class ContactoBeneficiario(db.Model):
     beneficiario_id= Column(Integer, ForeignKey('beneficiarios.id'))
 
     def __repr__(self):
-        return '<Beneficiario {}>'.format(self.nombre) 
+        return self.nombre 
+
+class Empresas(db.Model):
+    __tablename__ = 'empresas'
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50))
+    egresos = relationship("Egresos")
+
+    def __repr__(self):
+        return self.nombre 
 
 
+class CentrosNegocio(db.Model):
+    __tablename__ = 'centros_negocio'
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50))
+    detalles_egresos = relationship("DetallesEgreso")
+
+    def __repr__(self):
+        return self.nombre
+
+class Categorias(db.Model):
+    __tablename__ = 'categorias'
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50))
+    detalles_egresos = relationship("DetallesEgreso")
+
+    def __repr__(self):
+        return self.nombre
+
+
+class Conceptos(db.Model):
+    __tablename__ = 'conceptos'
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50))
+    detalles_egresos = relationship("DetallesEgreso")
+
+    def __repr__(self):
+        return self.nombre       
+
+class Cuentas(db.Model):
+    __tablename__ = 'cuentas'
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True)
+    banco = Column(String(50))
+    numero = Column(String(50))
+    saldo = Column(Numeric)    
+    pagos = relationship("Pagos")
+
+    def __repr__(self):
+        return self.banco 
+
+
+class FormasPago(db.Model):
+    __tablename__ = 'formas_pago'
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50))
+    pagos = relationship("Pagos")
+
+    def __repr__(self):
+        return self.nombre        
 #Event listeners
 def after_insert_listener(mapper, connection, target):
     # 'target' is the inserted object
--- app/egresos/routes.py
+++ app/egresos/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, request
 from flask_login import login_required
 from bcrypt import checkpw
 from app import db, login_manager
-from app.egresos.models import Egresos, DetallesEgreso, Pagos, Beneficiarios
+from app.egresos.models import *
 from datetime import date
 
 @blueprint.route('/<template>')
@@ -18,12 +18,12 @@ def capturar_egreso():
         montos = list(map(int, data.getlist("monto")))
         monto_total = sum(montos)
         print(data)
-        egreso = Egresos(beneficiario=data["beneficiario"], fecha_vencimiento=data["fecha_vencimiento"], 
+        egreso = Egresos(beneficiario_id=data["beneficiario"], fecha_vencimiento=data["fecha_vencimiento"], 
         fecha_programada_pago=data["fecha_programada_pago"], numero_documento=data["numero_documento"],
-        monto_total=monto_total, referencia=data["referencia"], empresa=data["empresa"], comentario=data["comentario"])
+        monto_total=monto_total, referencia=data["referencia"], empresa_id=data["empresa"], comentario=data["comentario"])
         for i in range(len(data.getlist("monto"))):
-            detalle = DetallesEgreso(centro_negocios=data.getlist("centro_negocios")[i], proveedor=data.getlist("proveedor")[i],
-            categoria=data.getlist("categoria")[i], concepto=data.getlist("concepto")[i], monto=data.getlist("monto")[i], 
+            detalle = DetallesEgreso(centro_negocios_id=data.getlist("centro_negocios")[i], proveedor_id=data.getlist("proveedor")[i],
+            categoria_id=data.getlist("categoria")[i], concepto_id=data.getlist("concepto")[i], monto=data.getlist("monto")[i], 
             numero_control=data.getlist("numero_control")[i], descripcion=data.getlist("descripcion")[i])
             egreso.detalles.append(detalle)
         if ('pagado' in data):
@@ -33,43 +33,70 @@ def capturar_egreso():
             else:
                  conciliado = False    
                  refrencia_conciliacion = ""
-            pago = Pagos(forma_pago = data["forma_pago"], referencia_pago = data["forma_pago"],fecha_pago = data["fecha_pago"], conciliado = conciliado, 
-            referencia_conciliacion= refrencia_conciliacion, empresa = data["empresa"], monto_total = data["monto_pagado"], comentario = data["comentario_pago"], beneficiario_id=1)        
+            pago = Pagos(forma_pago_id = data["forma_pago"], cuenta_id= data["cuenta_banco"], referencia_pago = data["forma_pago"],fecha_pago = data["fecha_pago"], conciliado = conciliado, 
+            referencia_conciliacion= refrencia_conciliacion,  monto_total = data["monto_pagado"], comentario = data["comentario_pago"], beneficiario_id=1)        
             egreso.pagos.append(pago)
         db.session.add(egreso)
         db.session.commit()
 
+    beneficiarios = Beneficiarios.query.all()
+    empresas= Empresas.query.all()
+    centros_negocios = CentrosNegocio.query.all()
+    proveedores = beneficiarios
+    categorias = Categorias.query.all()
+    conceptos = Conceptos.query.all()
+    cuentas_banco = Cuentas.query.all()
+    formas_pago = FormasPago.query.all()
 
-    formaPago = list(['Banamex','Santander','BBVA'])
-    vendor = list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
-    proveedor = list(['sub_categoria_1','sub_categoria_2','sub_categoria_3','sub_categoria_4','sub_categoria_5'])
-    categoria = list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
-    concepto= list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
-    
 
     return render_template("capturar_egreso.html",
                            navbar_data_capture = 'active',
                            title = "Registro de Egresos",
-                           formaPago = formaPago,
-                           vendor = vendor,
-                           proveedor = proveedor,
-                           categoria = categoria,
-                           concepto = concepto)
+                           beneficiarios = beneficiarios,
+                           empresas = empresas,
+                           centros_negocios = centros_negocios,
+                           proveedores = proveedores,
+                           categorias = categorias,
+                           conceptos = conceptos,
+                           cuentas_banco=cuentas_banco,
+                           formas_pago = formas_pago)
 
 
 @blueprint.route('cuentas_por_pagar', methods=['GET', 'POST'])
 def cuentas_por_pagar():
     egresos = Egresos.query.all()
+    for egreso in egresos:
+        egreso.beneficiario = Beneficiarios.query.get(egreso.beneficiario_id)
     return render_template("cuentas_por_pagar.html", egresos=egresos)
 
 @blueprint.route('/perfil_egreso/<int:egreso_id>', methods=['GET', 'POST'])
 def perfil_egreso(egreso_id):
     egreso = Egresos.query.get(egreso_id)
+    egreso.beneficiario= Beneficiarios.query.get(egreso.beneficiario_id)
+    egreso.empresa = Empresas.query.get(egreso.empresa_id)
     pagos = Pagos.query.join(Egresos.pagos, Beneficiarios).filter(Egresos.id == egreso_id)
     # No es eficiente muchos queries buscar como hacerlo en el query de arriba
     for pago in pagos:
         pago.beneficiario = Beneficiarios.query.get(pago.beneficiario_id)
+        pago.cuenta_banco = Cuentas.query.get(pago.cuenta_id)
 
     detalles = DetallesEgreso.query.filter(DetallesEgreso.egreso_id == egreso_id)
+    for detalle in detalles:
+        detalle.proveedor = Beneficiarios.query.get(detalle.proveedor_id)
+        detalle.centro_negocios = CentrosNegocio.query.get(detalle.centro_negocios_id)
+        detalle.categoria = Categorias.query.get(detalle.categoria_id)
+        detalle.concepto = Conceptos.query.get(detalle.concepto_id)
 
     return render_template("perfil_egreso.html", egreso=egreso, pagos=pagos, detalles=detalles)
+    
+
+ 
+@blueprint.route('/perfil_pago/<int:pago_id>', methods=['GET', 'POST'])
+def perfil_pago(pago_id):
+    pago = Pagos.query.get(pago_id)
+    pago.beneficiario= Beneficiarios.query.get(pago.beneficiario_id)
+    pago.forma_pago = FormasPago.query.get(pago.forma_pago_id)
+    pago.cuenta = Cuentas.query.get(pago.cuenta_id)
+    
+    return render_template("perfil_pago.html", pago=pago)
+   
