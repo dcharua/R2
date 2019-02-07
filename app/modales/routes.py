06e874791e94c97e6fd36af8908339313a8d615b
dcharua <danielcharua@hotmail.com>
2019-02-06 20:24:46 -0600

    modal beneficiarios

--- app/modales/routes.py
+++ app/modales/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, request
 from flask_login import login_required
 from bcrypt import checkpw
 from app import db, login_manager
+from app.db_models.models import *
 
 
 
@@ -10,3 +11,9 @@ from app import db, login_manager
 @login_required
 def route_template(template):
     return render_template(template + '.html')
+
+
+@blueprint.route('agregar_beneficiario', methods=['POST'])
+def agregar_beneficiario():
+    pagos = Pagos.query.all()
+    return redirect("/")
