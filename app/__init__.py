3baea38e878f63ec66adf64c7f7e66a9fffe02bd
dcharua <danielcharua@hotmail.com>
2019-03-24 14:40:32 -0600

    conciliaciones perfil_cuenta

--- app/__init__.py
+++ app/__init__.py
@@ -56,8 +56,8 @@ def create_app(config):
     DEBUG=True,
     LOGIN_DISABLED = True,
     SQLALCHEMY_TRACK_MODIFICATIONS = True,
-    SQLALCHEMY_DATABASE_URI ="mssql+pyodbc://%s:%s@%s/%s?driver=ODBC+Driver+17+for+SQL+Server" %(environ.get('DB_USER'), environ.get('DB_PASSWORD'), environ.get('DB_ADDRESS'), environ.get('DB_NAME'))
-    #SQLALCHEMY_DATABASE_URI ="mysql+pymysql://%s:%s@%s/%s" %(environ.get('DB_USER'), environ.get('DB_PASSWORD'), environ.get('DB_ADDRESS'), environ.get('DB_NAME'))
+    #SQLALCHEMY_DATABASE_URI ="mssql+pyodbc://%s:%s@%s/%s?driver=ODBC+Driver+17+for+SQL+Server" %(environ.get('DB_USER'), environ.get('DB_PASSWORD'), environ.get('DB_ADDRESS'), environ.get('DB_NAME'))
+    SQLALCHEMY_DATABASE_URI ="mysql+pymysql://%s:%s@%s/%s" %(environ.get('DB_USER'), environ.get('DB_PASSWORD'), environ.get('DB_ADDRESS'), environ.get('DB_NAME'))
     # Para Adrian
     #SQLALCHEMY_DATABASE_URI ="mysql+pymysql://%s:%s@%s/%s" %('root','Adri*83224647', 'localhost','R2')
 
--- app/conciliaciones/routes.py
+++ app/conciliaciones/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, request
 from flask_login import login_required
 from bcrypt import checkpw
 from app import db, login_manager
-
+from app.db_models.models import *
 
 
 @blueprint.route('/<template>')
@@ -14,20 +14,65 @@ def route_template(template):
 
 @blueprint.route('/capturar_conciliacion', methods=['GET', 'POST'])
 def capturar_conciliacion():
-    formaPago = list(['Banamex','Santander','BBVA'])
-    vendor = list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
-    proveedor = list(['sub_categoria_1','sub_categoria_2','sub_categoria_3','sub_categoria_4','sub_categoria_5'])
-    categoria = list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
-    concepto= list(['categoria_1','categoria_2','categoria_3','categoria_4','categoria_5'])
-    user_inputs = dict(request.form)
-    print(user_inputs)
+    cuentas = Cuentas.query.all()
+    for cuenta in cuentas:
+        cuenta.saldo_conciliado_egresos = 0
+        cuenta.saldo_conciliado_ingresos = 0
+        cuenta.saldo_transito_egresos = 0
+        cuenta.saldo_transito_ingresos = 0
+        cuenta.saldo_solicitado_egresos = 0 
+        for pago in cuenta.pagos:
+                if pago.status == 'conciliado':
+                    cuenta.saldo_conciliado_egresos += pago.monto_total
+                elif pago.status == 'por_conciliar':
+                    cuenta.saldo_transito_egresos += pago.monto_total
+                elif pago.status == 'solicitado':
+                    cuenta.saldo_solicitado_egresos += pago.monto_total
 
-    return render_template("capturar_conciliacion.html",
-                           navbar_data_capture = 'active',
-                           title = "Registro de Conciliaciones",
-                           formaPago = formaPago,
-                           vendor = vendor,
-                           proveedor = proveedor,
-                           categoria = categoria,
-                           concepto = concepto,
-                           velocity_max = 1)
+        for pago in cuenta.pagos_ingresos:
+                if pago.status == 'conciliado':
+                    cuenta.saldo_conciliado_ingresos += pago.monto_total
+                elif pago.status == 'por_conciliar':
+                    cuenta.saldo_transito_ingresos += pago.monto_total
+        cuenta.ultima_conciliacion = None
+        if (cuenta.conciliaciones):
+                cuenta.ultima_conciliacion = cuenta.conciliaciones[-1]
+    return render_template("capturar_conciliacion.html", cuentas=cuentas)   
+
+@blueprint.route('/agregar_cuenta', methods=['GET', 'POST'])
+@login_required
+def agregar_cuenta():
+    if request.form:
+        data = request.form
+        cuenta = Cuentas(nombre=data["nombre"], banco=data["banco"], numero=data["numero"], saldo=data["saldo"], saldo_inicial=data["saldo"], empresa_id=data["empresa"], comentario=data["comentario"], numero_cheque=0) 
+        db.session.add(cuenta)
+        db.session.commit()   
+        return redirect("/conciliaciones/capturar_conciliacion")    
+
+@blueprint.route('/perfil_cuenta/<int:cuenta_id>', methods=['GET', 'POST'])
+@login_required
+def perfil_cuenta(cuenta_id):
+    cuenta = Cuentas.query.get(cuenta_id)
+    saldo_conciliado_egresos = 0
+    saldo_conciliado_ingresos = 0
+    saldo_transito_egresos = 0
+    saldo_transito_ingresos = 0
+    saldo_solicitado_egresos = 0 
+    for pago in cuenta.pagos:
+        if pago.status == 'conciliado':
+            saldo_conciliado_egresos += pago.monto_total
+        elif pago.status == 'por_conciliar':
+            saldo_transito_egresos += pago.monto_total
+        elif pago.status == 'solicitado':
+            saldo_solicitado_egresos += pago.monto_total
+
+    for pago in cuenta.pagos_ingresos:
+        if pago.status == 'conciliado':
+            saldo_conciliado_ingresos += pago.monto_total
+        elif pago.status == 'por_conciliar':
+            saldo_transito_ingresos += pago.monto_total
+    ultima_conciliacion = None
+    if (cuenta.conciliaciones):
+        ultima_conciliacion = cuenta.conciliaciones[-1]
+    return render_template("perfil_cuenta.html", cuenta=cuenta,  saldo_conciliado_egresos=saldo_conciliado_egresos, saldo_conciliado_ingresos=saldo_conciliado_ingresos,  
+    saldo_transito_egresos =  saldo_transito_egresos, saldo_transito_ingresos=saldo_transito_ingresos, saldo_solicitado_egresos = saldo_solicitado_egresos, ultima_conciliacion = ultima_conciliacion)
